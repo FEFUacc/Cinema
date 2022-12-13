9fbff638f47d24f336584a4a287c51bd5d203a70
Maksim Kosenok <115632314+FEFUacc@users.noreply.github.com>
2022-12-13 22:49:37 +0000

    newFunction3

--- .ccls-cache/@home@runner@WiryTepidAssemblylanguage/main.c
+++ .ccls-cache/@home@runner@WiryTepidAssemblylanguage/main.c
@@ -1,6 +1,13 @@
 #include <stdio.h>
 
+int newFunction() { return 10; }
+
+int newFunction2() { return 10; }
+
+int newFunction3() { return 20; }
+
 int main(void) {
+  newFunction();
   printf("Hello World\n");
   return 0;
 }
--- main.c
+++ main.c
@@ -4,6 +4,8 @@ int newFunction() { return 10; }
 
 int newFunction2() { return 10; }
 
+int newFunction3() { return 20; }
+
 int main(void) {
   newFunction();
   printf("Hello World\n");
